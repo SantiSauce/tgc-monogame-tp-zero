0ff3982bfccd7e9f9723ba8cbad8b5dd02d48b35
SantiSauce <santiagosaucedo66@gmail.com>
2024-04-16 16:14:59 -0300

    Not done

--- TGC.MonoGame.TPZero/TGCGame.cs
+++ TGC.MonoGame.TPZero/TGCGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -7,17 +8,17 @@ using TGC.MonoGame.TP.Content.Models;
 
 namespace TGC.MonoGame.TP
 {
-    /// <summary>
-    ///     Clase principal del juego.
-    /// </summary>
     public class TGCGame : Game
+
+
     {
+
         public const string ContentFolder3D = "Models/";
         public const string ContentFolderEffects = "Effects/";
         public const string ContentFolderMusic = "Music/";
         public const string ContentFolderSounds = "Sounds/";
         public const string ContentFolderSpriteFonts = "SpriteFonts/";
-        public const string ContentFolderTextures = "Textures/";
+        public const string ContentFolderTextures = "Textures/"; 
 
         private GraphicsDeviceManager Graphics { get; }
         private CityScene City { get; set; }
@@ -25,112 +26,135 @@ namespace TGC.MonoGame.TP
         private Matrix CarWorld { get; set; }
         private FollowCamera FollowCamera { get; set; }
 
+   
 
-        /// <summary>
-        ///     Constructor del juego.
-        /// </summary>
+
         public TGCGame()
         {
-            // Se encarga de la configuracion y administracion del Graphics Device.
             Graphics = new GraphicsDeviceManager(this);
-
-            // Carpeta donde estan los recursos que vamos a usar.
             Content.RootDirectory = "Content";
-
-            // Hace que el mouse sea visible.
             IsMouseVisible = true;
         }
 
-        /// <summary>
-        ///     Llamada una vez en la inicializacion de la aplicacion.
-        ///     Escribir aca todo el codigo de inicializacion: Todo lo que debe estar precalculado para la aplicacion.
-        /// </summary>
         protected override void Initialize()
         {
-            // Enciendo Back-Face culling.
-            // Configuro Blend State a Opaco.
             var rasterizerState = new RasterizerState();
             rasterizerState.CullMode = CullMode.CullCounterClockwiseFace;
             GraphicsDevice.RasterizerState = rasterizerState;
             GraphicsDevice.BlendState = BlendState.Opaque;
 
-            // Configuro las dimensiones de la pantalla.
             Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
             Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
             Graphics.ApplyChanges();
 
-            // Creo una camaar para seguir a nuestro auto.
             FollowCamera = new FollowCamera(GraphicsDevice.Viewport.AspectRatio);
-
-            // Configuro la matriz de mundo del auto.
             CarWorld = Matrix.Identity;
 
             base.Initialize();
         }
 
-        /// <summary>
-        ///     Llamada una sola vez durante la inicializacion de la aplicacion, luego de Initialize, y una vez que fue configurado GraphicsDevice.
-        ///     Debe ser usada para cargar los recursos y otros elementos del contenido.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Creo la escena de la ciudad.
             City = new CityScene(Content);
-
-            // La carga de contenido debe ser realizada aca.
+            CarModel = Content.Load<Model>("Models/scene/car");
 
             base.LoadContent();
         }
 
-        /// <summary>
-        ///     Es llamada N veces por segundo. Generalmente 60 veces pero puede ser configurado.
-        ///     La logica general debe ser escrita aca, junto al procesamiento de mouse/teclas.
-        /// </summary>
+        private const float wheelForce = 4000f;
+        private const float carMass = 20f;
+
+        private const float GRAVITY = 5000f;
+        private const float JUMP = 1500f;
+        private bool _onGround = true;
+
+        private Vector3 carVelocity = Vector3.Zero;
+
+        float leftRotationAngle = MathHelper.ToRadians(30f);
+        float rightRotationAngle = -MathHelper.ToRadians(30f);
+
         protected override void Update(GameTime gameTime)
         {
-            // Caputo el estado del teclado.
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float totalForce = 0;
+
             var keyboardState = Keyboard.GetState();
+
             if (keyboardState.IsKeyDown(Keys.Escape))
             {
-                // Salgo del juego.
                 Exit();
             }
 
-            // La logica debe ir aca.
+            if (keyboardState.IsKeyDown(Keys.A))
+            {
+                CarWorld = Matrix.CreateFromAxisAngle(CarWorld.Up, leftRotationAngle * dt) * CarWorld;
+            }
 
-            // Actualizo la camara, enviandole la matriz de mundo del auto.
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                CarWorld = Matrix.CreateFromAxisAngle(CarWorld.Up, rightRotationAngle * dt) * CarWorld;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                totalForce = wheelForce;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                totalForce = -wheelForce;
+            }
+
+            if (!_onGround)
+            {
+                carVelocity.Y += -GRAVITY * dt;
+            }
+
+            if (CarWorld.Translation.Y <= 0)
+            {
+                _onGround = true;
+                carVelocity.Y = 0;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Space) && _onGround)
+            {
+                carVelocity.Y = JUMP;
+                _onGround = false;
+            }
+
+            Vector3 forwardDirection = CarWorld.Forward;
+            Vector3 forwardAcceleration = forwardDirection * totalForce / carMass;
+
+            carVelocity += (forwardAcceleration * dt);
+
+            Matrix translationMatrix = Matrix.CreateTranslation(carVelocity * dt);
+            CarWorld *= translationMatrix;
+
             FollowCamera.Update(gameTime, CarWorld);
 
             base.Update(gameTime);
         }
 
 
-        /// <summary>
-        ///     Llamada para cada frame.
-        ///     La logica de dibujo debe ir aca.
-        /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // Limpio la pantalla.
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // Dibujo la ciudad.
             City.Draw(gameTime, FollowCamera.View, FollowCamera.Projection);
 
-            // El dibujo del auto debe ir aca.
+            CarModel.Draw(CarWorld, FollowCamera.View, FollowCamera.Projection);
 
             base.Draw(gameTime);
         }
 
-        /// <summary>
-        ///     Libero los recursos cargados.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // Libero los recursos cargados dessde Content Manager.
             Content.Unload();
 
             base.UnloadContent();
         }
     }
 }
+
+
+
